945c83b77172afd81ef0db3282153615e0d110ad
Dennis Vuong <vuong.dennis@gmail.com>
2019-12-09 23:03:36 -0800

    Initial Commit

---
+++ Sudoku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sudoku
+//
+//  Created by Dennis Vuong on 12/9/19.
+//  Copyright © 2019 Dennis Vuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
